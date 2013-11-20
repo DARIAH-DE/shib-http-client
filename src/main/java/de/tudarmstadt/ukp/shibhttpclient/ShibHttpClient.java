1525a43c562c5665bb6ebf53e57b6a711fb4f6ee
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-11-20 13:05:43 +0100

    Updated license headers and acknowledgements.

--- src/main/java/de/tudarmstadt/ukp/shibhttpclient/ShibHttpClient.java
+++ src/main/java/de/tudarmstadt/ukp/shibhttpclient/ShibHttpClient.java
@@ -1,10 +1,4 @@
 /*******************************************************************************
- * Copyright 2013
- * Ubiquitous Knowledge Processing (UKP) Lab
- * Technische Universität Darmstadt
- *
- * Copyright 2013 Diamond Light Source Ltd.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,6 +10,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * For copyright information, see NOTICE.txt file.
  ******************************************************************************/
 
 package de.tudarmstadt.ukp.shibhttpclient;
